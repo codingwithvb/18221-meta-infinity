e55ca4f6cd0074227a2eb561f3ce3e5b18e1a4f2
Vedanta Bhargava <vedanta.bhargava@gmail.com>
2022-11-20 18:08:29 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad.java
@@ -0,0 +1,205 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Gamepad", group="Linear Opmode")
+//@Disabled
+public class Gamepad extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    // private DcMotor leftDrive = null;
+    // private DcMotor rightDrive = null;
+
+    DcMotor FrontRight;
+    DcMotor FrontLeft;
+    DcMotor BackRight;
+    DcMotor BackLeft;
+
+    DcMotor leftIntakeMotor;
+    DcMotor rightIntakeMotor;
+    DcMotor rightSlide;
+    DcMotor leftSlide;
+
+    Servo clawServo;
+    Servo armServo;
+
+    double clawServoPosition = 0.0;
+    double armServoPosition = 1.0;
+
+    double horizontal;
+    double vertical;
+    double pivot;
+    double sensitivity;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        FrontLeft  = hardwareMap.dcMotor.get("LeftFront");
+        FrontRight = hardwareMap.dcMotor.get("RightFront");
+        BackRight = hardwareMap.dcMotor.get("RightBack");
+        BackLeft = hardwareMap.dcMotor.get("LeftBack");
+
+        leftIntakeMotor = hardwareMap.dcMotor.get("LeftIntake");
+        rightIntakeMotor = hardwareMap.dcMotor.get("RightIntake");
+
+        leftSlide = hardwareMap.dcMotor.get("LeftSlide");
+        rightSlide = hardwareMap.dcMotor.get("RightSlide");
+
+        armServo = hardwareMap.servo.get("ArmServo");
+        armServo.setPosition(armServoPosition);
+        clawServo = hardwareMap.servo.get("ClawServo");
+        clawServo.setPosition(clawServoPosition);
+
+        FrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        BackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            horizontal = gamepad1.left_stick_x;
+            pivot = gamepad1.right_stick_x;
+            vertical = -gamepad1.left_stick_y;
+            sensitivity = gamepad1.left_trigger;
+            if (sensitivity > 0) {
+                vertical = (float) (vertical * 0.5);
+                horizontal = (float) (horizontal * 0.5);
+                pivot = (float) (pivot * 0.5);
+            }
+            FrontRight.setPower(-pivot + (vertical - horizontal));
+            BackRight.setPower(-pivot + vertical + horizontal);
+            FrontLeft.setPower(pivot + vertical + horizontal);
+            BackLeft.setPower(pivot + (vertical - horizontal));
+
+            //intake power will be constant
+
+            if (gamepad2.right_bumper) {
+                leftSlide.setPower(0.5);
+                rightSlide.setPower(0.5);
+            }
+            else {
+                leftSlide.setPower(0);
+                rightSlide.setPower(0);
+            }
+            if (gamepad2.left_bumper) {
+                leftSlide.setPower(-0.5);
+                rightSlide.setPower(-0.5);
+            }
+            else {
+                leftSlide.setPower(0);
+                rightSlide.setPower(0);
+            }
+            if (gamepad2.b){
+                armServoPosition = 0.0;
+                armServo.setPosition(armServoPosition);
+            }
+            else if (gamepad2.y){
+                armServoPosition = 1.0;
+                armServo.setPosition(armServoPosition);
+            }
+            if (gamepad2.a){
+                clawServoPosition = 0.0;
+                clawServo.setPosition(clawServoPosition);
+            }
+            else if (gamepad2.x){
+                clawServoPosition = 1.0;
+                clawServo.setPosition(clawServoPosition);
+            }
+            if(gamepad1.right_trigger>0){
+                rightIntakeMotor.setPower(1.0);
+                leftIntakeMotor.setPower(-1.0);
+            }
+            else
+            {
+                rightIntakeMotor.setPower(0.0);
+                leftIntakeMotor.setPower(0.0);
+            }
+            if(gamepad1.left_trigger>0){
+
+
+                rightIntakeMotor.setPower(-1.0);
+                leftIntakeMotor.setPower(1.0);
+            }
+            else
+            {
+                rightIntakeMotor.setPower(0.0);
+                leftIntakeMotor.setPower(0.0);
+            }
+            if(gamepad2.b){
+                myGoToHeightPOS(-800,1);
+            }else{
+                telemetry.update();
+            }
+        }
+    }
+    private void myGoToHeightPOS(int slidePOS, double motorPower) {
+        leftSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftSlide.setTargetPosition((slidePOS));
+        rightSlide.setTargetPosition(slidePOS);
+        leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftSlide.setPower(motorPower);
+        rightSlide.setPower(motorPower);
+        leftSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+}
